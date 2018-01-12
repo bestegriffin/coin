a3134bdbe8592b40ba331ac35487dbf7c0822cb4
bestegriffin <griffinbeste@gmail.com>
2018-01-11 22:58:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ app.use(function(req, res, next) {
     next();
 });
 
-// // mongodb connection
+// mongodb connection
 // development db
 // mongoose.connect("mongodb://localhost:27017/coinSense", { useMongoClient: true });
 
-// // deployment db
+// deployment db
 mongoose.connect("mongodb://heroku_q941fpr3:o956fhe81vo4v82dpfupnc92js@ds251277.mlab.com:51277/heroku_q941fpr3");
 
 var db = mongoose.connection;
